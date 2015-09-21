a0466c9fc5b3b3a278877cae2aafe0816214711b
onwebchat_dev <onwebchat_dev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-09-21 05:12:05 +0000

    documentation update
    git-svn-id: https://plugins.svn.wordpress.org/onwebchat/tags/1.0.13@1249994 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ onwebchat.php
@@ -0,0 +1,111 @@
+<?php
+/*
+Plugin Name: onWebChat Live Chat
+Plugin URI: https://www.onwebchat.com/wp_plugin.php
+Description: onWebChat is a live chat system, that helps you communicate with your website's visitors.
+Author: onWebChat
+Version: 1.0.1
+Author URI: https://www.onwebchat.com
+*/
+
+// get logo url
+define('ONWEBCHAT_SMALL_LOGO', plugins_url( 'images/onwebchat-logo.png' , __FILE__ ));
+
+// call function on hook admin_menu
+add_action('admin_menu','onwebchat_setup_menu');
+
+// define the settings
+add_action('admin_init','onwebchat_register_setttings');
+
+// add widget plugin to the footer
+add_action('wp_footer', 'onwebchat_add_script_at_footer');
+
+// add menu page on admin menu with function onwebchat_init_menu_page()
+function onwebchat_setup_menu() {
+	add_menu_page( 'onWebChat Settings', 'onWebChat', 'administrator', 'onwebchat_settings', 'onwebchat_init_menu_page', ONWEBCHAT_SMALL_LOGO);
+}
+
+// display admin option page
+function onwebchat_init_menu_page() {
+?>
+<div>
+	<h2>onWebChat Settings Page</h2>
+</br>
+	<form action="options.php" method="post">
+		<?php settings_fields('onwebchat_plugin_option'); ?>
+		<!-- prints all settings inputs we need -->
+		<?php do_settings_sections('onwebchat_plugin'); ?>
+		<input name="Submit" type="submit" value="<?php esc_attr_e('Save Changes'); ?>" />
+	</form>
+</div>
+<?php
+}
+
+// register the settings to store data
+function onwebchat_register_setttings() {
+	register_setting( 'onwebchat_plugin_option', 'onwebchat_plugin_option', 'plugin_options_validate' );
+	//* plugin
+	add_settings_section('plugin_main', 'To install Chat Widget:', 'onwebchat_section_text', 'onwebchat_plugin');
+	//* plugin
+	add_settings_field('plugin_text_string', 'Chat Id:', 'onwebchat_setting_field_text', 'onwebchat_plugin', 'plugin_main');
+}
+
+// prints the text of sections
+function onwebchat_section_text() {
+	echo '<p>If you don\'t have an account on onWebChat live chat service create one <a href="https://www.onwebchat.com/signup.php" target="_blank">here</a></p>'.
+        '<p>To install onWebChat on your Wordpress site please insert the Chat Id to the following field.</p>'.
+	'<p>You will find Chat Id in settings-page of onWebChat admin, login <a href="https://www.onwebchat.com/login.php" target="_blank">here</a> </p>'.
+    '<p>[You can customize chat widget appearance from settings admin page]</p>';
+}
+
+// print the text of section field
+function onwebchat_setting_field_text() {
+	$options = get_option('onwebchat_plugin_option');
+	echo "<input id='plugin_text_string' name='onwebchat_plugin_option[text_string]' size='40' type='text' value='{$options['text_string']}' />";
+}
+
+// validate data input
+function plugin_options_validate($input) {
+	$newinput['text_string'] = trim($input['text_string']);
+
+	if(!preg_match('/^[a-f0-9\\/]{35,50}$/i', $newinput['text_string'])) {
+        $newinput['text_string'] = '';
+    }
+
+	return $newinput;
+}
+
+// print chat widget code
+function onwebchat_add_script_at_footer() {
+
+	$options = get_option('onwebchat_plugin_option');
+	$chatId = $options['text_string'];
+    
+    $widgetCode = "<script type='text/javascript'>
+    var onWebChat={ar:[], set: function(a,b){if (typeof onWebChat_==='undefined'){this.ar.
+    push([a,b]);}else{onWebChat_.set(a,b);}},get:function(a){return(onWebChat_.get(a));},w
+    :(function(){ var ga=document.createElement('script'); ga.type = 'text/javascript';ga.
+    async=1;ga.src='//www.onwebchat.com/clientchat/$chatId';
+    var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(ga,s);})()}
+    </script>";
+    
+
+    //check if user is logged if yes get username, email (all users registered have them)
+    if ( is_user_logged_in() ) { 
+
+    	global $current_user;
+    	get_currentuserinfo();
+
+	    // get user name
+	    $onwebchat_user = $current_user->user_login;
+
+	    // get user email
+	    $onwebchat_email = $current_user->user_email;
+
+	    //add api info
+    	$widgetCode = $widgetCode."<script type='text/javascript'>onWebChat.set('name','$onwebchat_user');onWebChat.set('email','$onwebchat_email'); </script>";
+   	}
+    
+    echo $widgetCode;
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,87 @@
+=== onWebChat - Free Live Chat ===
+Contributors: onwebchat_dev
+Tags: live chat, live support, chat plugin, online chat, support plugin, contact us, customer support, free chat, chat software, live chat service, live chat software, live chatting, live help, live web chat, livechat, online support, php live chat, html chat, javascript chat, support software, website chat, wordpress chat, wordpress live chat, wordpress live chat plugin, onwebchat live chat, onWebChat
+Requires at least: 3.1
+Tested up to: 4.3
+Stable tag: 1.0.13
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Free Live Chat plugin for your website. Install it in less than a minute, and chat with your visitors!
+
+== Description ==
+
+Chat with your website visitors in real-time, monitor site traffic, provide better support and increase your sales!
+
+onWebChat plugin for WordPress provides an easy way to integrate your WordPress website with our innovative live chat system.
+
+100% free plan available for ever.
+Plus, we're offering our Premium plan for free until 30th November 2015!
+
+Start now! It takes less than a minute, just install the plugin and sign up for our service: http://www.onwebchat.com
+
+
+FEATURES: 
+
+* Fully customizable chat widget (colors, size, texts)
+* Visitor monitoring - keep track of your visitors
+* Chat with any of your website visitor
+* Geolocation data of visitors
+* Block/unblock visitors
+* Text shortcuts for common questions
+* Full chat transcript history 
+* Multiple operators 
+* Install on multiple websites
+* Chat widget language customization
+* Sound / Desktop notifications 
+* Offline form, when you are not online
+* Easy File Sharing with your visitors
+* SSL/Data security
+* Free plan available for ever
+
+You need any assistance? Feel free to chat with us on [onWebChat](https://www.onwebchat.com "onWebChat Home Page")
+
+== Installation ==
+
+1. [Create](https://www.onwebchat.com/signup.php "onWebChat Sign Up") an account on onWebChat.
+2. Go to Settings page and copy the "Chat Id".
+3. Install and activate the onWebChat plugin.
+4. Paste the "Chat Id" to the field and Click "Save Changes".
+
+== Frequently Asked Questions ==
+
+= Do I have to pay for onWebChat? =
+
+No, there is a free plan that you can use for ever.
+After 1st December, 2 low cost plans will be available.
+
+= Do I have to install any extra software? =
+
+No, you have to install this plugin and just sign up on onWebChat service.
+
+= How can I change the appearance of the widget on my page? =
+
+You can change all your widget settings from your [onWebChat Admin Page](https://www.onwebchat.com/login.php "onWebChat Admin Page").
+
+= Is it possible to translate any text on the chat widget? =
+
+Yes, all texts are translatable and you can set your own language and texts you like.
+
+= Can I initiate a chat before the visitor does? =
+
+Yes, simply click on a visitor from the visitors list.
+
+= Can I hide chat’s widget when I am offline? =
+
+Yes, just check the "hide chat widget.." in your admin page.
+The chat widget will be hidden when all operators are offline
+or have select "invisible" status.
+
+== Screenshots ==
+
+1. onWebChat widget on WordPress website
+
+2. Operator chatting with a visitor
+
+3. Operator dashboard main page
+
